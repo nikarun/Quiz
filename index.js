0267ba4391e7a6d4cd9b4bb3083cf022c0807171
Arun Parihar <parihararun3@gmail.com>
2021-05-31 16:04:44 +0530

    improved UI part added options

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var userName=readlineSync.question("Whats ur name?")
 console.log("hey "+userName+" Welcome to DO YOU KNOW TCS?");
 var score=0;
 var totalScore=0;
+var exit=false;
 function play(question,answer){
     var q1=readlineSync.question(question);
-    if((typeof(q1)==="string") && q1.toLowerCase()===answer.toLowerCase()){
+    if(q1==='e') exit=true;
+    else if((typeof(q1)==="string") && q1.toLowerCase()===answer.toLowerCase()){
         console.log("Right!");
         ++score;
     }
@@ -15,26 +17,48 @@ function play(question,answer){
     }
     console.log("current score is "+score);
         console.log("---------");
+        console.log("Press e to exit");
 }
-var questions=[{question:"in which year was TCS established?",
-answer:"1968"
+var questions=[
+    {question:`in which year was TCS established?
+a 1968
+b 1978
+c 1965
+`,
+answer:"a"
 },{
-    question:"Who is the founder of tcs?",
-    answer:"J.R.D Tata"
+    question:`Who is the founder of tcs?
+     a Ratan Tata
+     b J.R.D Tata
+     c Ajim Premji
+     `,
+    answer:"b"
 },{
-    question:"Who was the 2nd chairman of tata group?",
-    answer:"Jamsetji Nusserwanji Tata"
+    question:`Who was the 2nd chairman of tata group?
+    a Ratan Tata
+    b Jamsetji Nusserwanji Tata
+    c Faquir Chand Kohli
+    `,
+    answer:"b"
 },
 {
-    question:"When did TCS had been Ranked No. 1 in the BT 500 - India's Most Valuable Companies?",
-    answer:"2017"
+    question:`When did TCS had been Ranked No. 1 in the BT 500 - India's Most Valuable Companies?
+    a 2011
+    b 2018
+    c 2017
+    `,
+    answer:"c"
 },{
-    question:"What is the current revenue in USD for Q4 FY18 with employee strength of 3,94,998?",
-    answer:"4.972 Billion"
+    question:`What is the current revenue in USD for Q4 FY18 with employee strength of 3,94,998?
+    a 4.972 Billion
+    b 8.89 Billion
+    c 3.56 Billion
+    `,
+    answer:"a"
 }];
 totalScore=questions.length;
 for(let i=0;i<questions.length; i++){
-    play(questions[i].question,questions[i].answer);
+    if(!exit) play(questions[i].question,questions[i].answer);
 }
 console.log("your Final score is "+score+" out of "+totalScore);
 
